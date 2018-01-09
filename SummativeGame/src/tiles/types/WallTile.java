2c87736c8e86e0209dde4f8bf5018320bc01a64e
poweg9141 <poweg9141@HRH-IC0027322.wrdsb.ca>
2018-01-09 10:15:13 -0500

    finished, and working

--- SummativeGame/src/tiles/types/WallTile.java
+++ SummativeGame/src/tiles/types/WallTile.java
@@ -1,5 +1,6 @@
 package tiles.types;
 
+import entities.mobs.Player;
 import tiles.Tile;
 
 /**
@@ -10,11 +11,37 @@ import tiles.Tile;
 public class WallTile extends Tile{
 
 	/**
-	 * used ot create a tile that is solid
+	 * use it create a tile that is solid
 	 * @param id the id of the tile
 	 */
 	public WallTile(int id) {
 		super(id, true);
 	}
+        
+
+        public void update(Player p){
+            //gets the players x, y ,width, and height
+            float px = p.getX();
+            float py = p.getY();
+            int pw = p.getWidth();
+            int ph = p.getHeight();
+            float pdx = 0;
+            float pdy = 0;
+            if(px + pw >= x && px + pw <= x + STANDARD_DIAMETER){
+                if(py + ph >= y && py + ph <= y + STANDARD_DIAMETER){
+                    pdy -= (y - (py + ph));
+                }
+                else if(py <= y + STANDARD_DIAMETER && py >= y){
+                    pdy += (y - py);
+                }
+            }
+            else if(px <= x + STANDARD_DIAMETER && px >= x){
+                if(){
+                    
+                }
+                else if(){
+                    
+                }   
+            }                
 
 }
