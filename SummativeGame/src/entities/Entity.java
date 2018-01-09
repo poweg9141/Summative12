221efd42e7172775220d7f23a210e41c3fa20795
Graham <Graham@Master-PC>
2018-01-09 18:28:36 -0500

    added camera

--- SummativeGame/src/entities/Entity.java
+++ SummativeGame/src/entities/Entity.java
@@ -1,73 +1,80 @@
 package entities;
 
+import coreEngine.Game;
 import java.awt.Graphics;
 
 /**
  * anything in that game that isnt just an image
+ *
  * @author Graham
  *
  */
 public abstract class Entity {
 
-	//the entitys x and y position
-	protected float x, y;
-	//the entitys width and height
-	protected int width, height;
-	
-	/**
-	 * creates an entity
-	 * @param x the x position of the entity on the screen
-	 * @param y the y position of the entity on the screen
-	 * @param width the width of the entity in pixels
-	 * @param height the height of the entity in pixels
-	 */
-	public Entity(float x, float y, int width, int height){
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
-	}
-	
-	/**
-	 * used to update the entitys variables every frame
-	 */
-	public  abstract void tick();
-	
-	/**
-	 * used to render the entity onto the screen every frame
-	 * @param g the graphics object of the game the entity is being rendered in
-	 */
-	public abstract void render(Graphics g);
+    //stores the game the entity is in
+    protected Game game;
+    //the entitys x and y position
+    protected float x, y;
+    //the entitys width and height
+    protected int width, height;
 
-	public float getX() {
-		return x;
-	}
+    /**
+     * creates an entity
+     *
+     * @param x the x position of the entity on the screen
+     * @param y the y position of the entity on the screen
+     * @param width the width of the entity in pixels
+     * @param height the height of the entity in pixels
+     */
+    public Entity(Game game, float x, float y, int width, int height) {
+        this.game = game;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
 
-	public void setX(float x) {
-		this.x = x;
-	}
+    /**
+     * used to update the entitys variables every frame
+     */
+    public abstract void tick();
 
-	public float getY() {
-		return y;
-	}
+    /**
+     * used to render the entity onto the screen every frame
+     *
+     * @param g the graphics object of the game the entity is being rendered in
+     */
+    public abstract void render(Graphics g);
 
-	public void setY(float y) {
-		this.y = y;
-	}
+    public float getX() {
+        return x;
+    }
 
-	public int getWidth() {
-		return width;
-	}
+    public void setX(float x) {
+        this.x = x;
+    }
 
-	public void setWidth(int width) {
-		this.width = width;
-	}
+    public float getY() {
+        return y;
+    }
 
-	public int getHeight() {
-		return height;
-	}
+    public void setY(float y) {
+        this.y = y;
+    }
 
-	public void setHeight(int height) {
-		this.height = height;
-	}
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
