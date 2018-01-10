734255c9f3908fd3a666af11b256485e2c2f2bd4
poweg9141 <poweg9141@HRH-IC0027322.wrdsb.ca>
2018-01-10 09:19:19 -0500

    finished, and working

--- SummativeGame/src/tiles/types/WallTile.java
+++ SummativeGame/src/tiles/types/WallTile.java
@@ -16,32 +16,5 @@ public class WallTile extends Tile{
 	 */
 	public WallTile(int id) {
 		super(id, true);
-	}
-        
-
-        public void update(Player p){
-            //gets the players x, y ,width, and height
-            float px = p.getX();
-            float py = p.getY();
-            int pw = p.getWidth();
-            int ph = p.getHeight();
-            float pdx = 0;
-            float pdy = 0;
-            if(px + pw >= x && px + pw <= x + STANDARD_DIAMETER){
-                if(py + ph >= y && py + ph <= y + STANDARD_DIAMETER){
-                    pdy -= (y - (py + ph));
-                }
-                else if(py <= y + STANDARD_DIAMETER && py >= y){
-                    pdy += (y - py);
-                }
-            }
-            else if(px <= x + STANDARD_DIAMETER && px >= x){
-                if(){
-                    
-                }
-                else if(){
-                    
-                }   
-            }                
-
+	}            
 }
