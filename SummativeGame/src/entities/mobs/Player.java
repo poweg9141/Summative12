136bc61859a8963a7c4e448effb8b6b2cae07f78
Graham <Graham@Master-PC>
2018-01-21 18:04:03 -0500

    player animation

--- SummativeGame/src/entities/mobs/Player.java
+++ SummativeGame/src/entities/mobs/Player.java
@@ -4,7 +4,9 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import coreEngine.Game;
 import graphics.Camera;
+import graphics.SpriteSheet;
 import java.awt.Color;
+import utils.Time;
 
 /**
  * the player
@@ -16,9 +18,13 @@ public class Player extends Mob {
 
     //stores the players image
     private BufferedImage player;
+    private BufferedImage[] player_left, player_right;
     //stores an instance of the game the player is in
     private Game game;
     private float dx, dy;
+    private int index;
+    private long lastTime;
+    private double desiredTimeBetweenFrame = 0.5;
 
     /**
      * to create a player for the game
@@ -28,9 +34,16 @@ public class Player extends Mob {
      * @param x the x position of the player on the screen
      * @param y the y position of the player on the screen
      */
-    public Player(Game game, BufferedImage player, float x, float y) {
+    public Player(Game game, SpriteSheet playerSheet, float x, float y) {
         super(game, x, y, Mob.STANDARD_DIAMETER, Mob.STANDARD_DIAMETER);
-        this.player = player;
+        player_left = new BufferedImage[2];
+        player_left[0] = playerSheet.getSpriteAtIndex(2);
+        player_left[1] = playerSheet.getSpriteAtIndex(3);
+        player_right = new BufferedImage[2];
+        player_right[0] = playerSheet.getSpriteAtIndex(0);
+        player_right[1] = playerSheet.getSpriteAtIndex(1);
+        player = player_right[0];
+        index = 0;
         this.game = game;
         bounds.setBounds(24, 45, 14, 19);
         health = 100;
@@ -46,6 +59,20 @@ public class Player extends Mob {
             game.getCamera().framEntity(this);
         }else{
             handleDeath();
+        }
+        if(lastTime == 0){
+            lastTime = Time.getCurrentTime();
+        }
+        if(Time.hasBeenTime(lastTime, desiredTimeBetweenFrame)){
+            index++;
+            if(index > 1){
+                index = 0;
+            }
+        }
+        if(dx < 0){
+            player = player_left[index];
+        }else if(dx > 0){
+            player = player_right[index];
         }
     }
 
