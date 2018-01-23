1619b3e802154935c688edd48ebcede337bbf032
pistm9061 <pistm9061@HRH-IC0027323.wrdsb.ca>
2018-01-23 09:32:22 -0500

    yuuuuuuh

--- SummativeGame/src/coreEngine/GameVariables.java
+++ SummativeGame/src/coreEngine/GameVariables.java
@@ -16,9 +16,9 @@ public class GameVariables {
 	private static double playerWidth = 64;
 	private static double playerHeight = 64;
 	//the speed of a player
-	private static double playerSpeed = 0.02;
+	private static double playerSpeed = 0.2;
         //stores the speed of the enemies
-        private static double entitySpeed = 0.01;
+        private static double entitySpeed = 0.1;
 	//stores the default name and score for the high scores text file, used when creating a new file
 	//NOTE: default name must have NO spaces
 	private static final String defaultHighScoreName = "WendyWako";
